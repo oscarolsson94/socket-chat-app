411900c2b0d05527f2a490072bd5718ce3f02db7
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-03-23 20:02:11 +0100

    basic config

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,20 @@
-console.log("this is from app");
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+import config from "config";
+
+const port = config.get<number>("port");
+const host = config.get<string>("host");
+const corsOrigin = config.get<string>("corsOrigin");
+
+const app = express();
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: corsOrigin,
+        credentials: true,
+    },
+});
