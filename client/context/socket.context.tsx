2eadee16524e752cc6d503df9aa6c810713fc6ad
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-03-26 14:47:46 +0100

    user context

--- client/context/socket.context.tsx
+++ client/context/socket.context.tsx
@@ -1,13 +1,35 @@
-import { createContext, useContext } from "react";
-import { io } from "socket.io-client";
+import {
+    createContext,
+    Dispatch,
+    SetStateAction,
+    useContext,
+    useState,
+} from "react";
+import { io, Socket } from "socket.io-client";
 import { SOCKET_URL } from "../config/default";
+
+interface Context {
+    socket: Socket;
+    username?: string;
+    setUsername: Dispatch<SetStateAction<string>>;
+}
 
 const socket = io(SOCKET_URL);
 
-const SocketContext = createContext({ socket });
+const SocketContext = createContext<Context>({
+    socket,
+    setUsername: () => "",
+});
 
 export const SocketsProvider = (props: any) => {
-    return <SocketContext.Provider value={{ socket }} {...props} />;
+    const [username, setUsername] = useState("");
+
+    return (
+        <SocketContext.Provider
+            value={{ socket, username, setUsername }}
+            {...props}
+        />
+    );
 };
 
 export const useSockets = () => useContext(SocketContext);
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -4,9 +4,21 @@ import styles from "../styles/Home.module.css";
 import { useSockets } from "../context/socket.context";
 import { RoomsContainer } from "../containers/Rooms";
 import { MessagesContainer } from "../containers/Messages";
+import { useRef } from "react";
 
 export default function Home() {
-    const { socket } = useSockets();
+    const { socket, username, setUsername } = useSockets();
+    const usernameRef = useRef(null);
+
+    const handleSetUser = () => {
+        const value = usernameRef.current.value;
+
+        if (!value) return;
+
+        setUsername(value);
+
+        localStorage.setItem("username", value);
+    };
     return (
         <div>
             <RoomsContainer />
