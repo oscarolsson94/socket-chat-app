cb2434f84aa574dae3c8802f9a1cfa2a12374ec5
Oscar Olsson <oscar.olsson94@hotmail.com>
2022-03-26 14:39:13 +0100

    rendering containers

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,8 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { useSockets } from "../context/socket.context";
+import { RoomsContainer } from "../containers/Rooms";
+import { MessagesContainer } from "../containers/Messages";
 
 export default function Home() {
     const { socket } = useSockets();
-    return <p>{socket.id}</p>;
+    return (
+        <div>
+            <RoomsContainer />
+            <MessagesContainer />
+        </div>
+    );
 }
